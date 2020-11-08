5395e794bf4917abef8fcfa944fab50aa2606ffd
Anastasia Lysenko <nastya@lysenko.ms>
2020-11-09 00:18:44 +0300

    improved confusion matrix

--- lambdata_nastyalolpro/__init__.py
+++ lambdata_nastyalolpro/__init__.py
@@ -1,4 +1,5 @@
 from .df_utils import *
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
+author_name = "Anastasia Lysenko"
 my_str = "This is my_str from __init__.py"
--- lambdata_nastyalolpro/df_utils.py
+++ lambdata_nastyalolpro/df_utils.py
@@ -2,22 +2,23 @@ import pandas as pd
 import numpy as np
 import random
 
-#report confusion matrix with labels
+# report confusion matrix with labels
+
 
 def confusion_matrix(predicted, true):
-    
+
     if len(predicted) != len(true):
         print("Error: lengths of labels do not match")
 
     else:
-        d = {'predicted':predicted, 'true':true}
+        d = {'predicted': predicted, 'true': true}
         df = pd.DataFrame(data=d)
-        
-        tp = []
-        tn = []
-        fp = []
-        fn = []
-   
+
+        tp = 0
+        tn = 0
+        fp = 0
+        fn = 0
+
         for row in range(len(df)):
             if df['predicted'].iloc[row] == df['true'].iloc[row]:
                 if df['predicted'].iloc[row] == 1:
@@ -29,30 +30,36 @@ def confusion_matrix(predicted, true):
                     fp += 1
                 else:
                     fn += 1
-          
-        d = {' ':['Predicted Positive (1)', 'Predicted Negetive (0)'], 'Actually                   Positive (1)': [tp, fn], 'Acyually Negative (0)': [fp, tn]}
+
+        d = {' ': ['Predicted Positive (1)', 'Predicted Negetive (0)'],
+             'Actually Positive (1)': [tp, fn],
+             'Acyually Negative (0)': [fp, tn]}
 
         df = pd.DataFrame(data=d)
+        
         return df
+        
 
 
-#"Generate more data" function, takes dataframes and makes more rows
+
+
+# "Generate more data" function, takes dataframes and makes more rows
+
 
 def more_data(df, num_rows):
-   
+
     d = {}
 
     for col in df:
-        
+
         column = list(df[col])
         for i in range(num_rows):
 
             num = random.randint(0, len(df))
             new_val = df[col].iloc[num]
             column.append(new_val)
-        
-        d[col] = column  
-    
-    df = pd.DataFrame(data = d)
-    
+
+        d[col] = column
+    df = pd.DataFrame(data=d)
+
     return df
